24a38f2a96846cd80ff23989ec5958c8adadd0dc
oysbak <64131013+oysbak@users.noreply.github.com>
2020-06-25 11:11:55 +0200

    Initial commit

---
+++ src/summerschool/Main.java
@@ -0,0 +1,9 @@
+package summerschool;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello summer school!");
+    }
+}
